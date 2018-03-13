720bdf8a3870ba468e660f3f9d43d59ac8449542
Andrew Locke <alocke12992@gmail.com>
2018-03-13 10:59:54 -0600

    Adds materialize

--- src/App.js
+++ src/App.js
@@ -1,21 +1,11 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+import TodoForm from './components/TodoForm';
+import List from './components/List';
+import Footer from './components/Footer';
+const App = () => (
+  <div className="App">
+  </div>
+)
 
 export default App;
